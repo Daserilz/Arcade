f24bee7aa525e73ccf7230e54b350065dda6d702
Daserilz <knjirapat@gmail.com>
2026-06-29 23:21:44 +0700

    Edit สองตัวละครmove ใน 1 script

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     public Transform cameraTransform;
 
+
+    private Vector2 moveInput;
+
+
+    public void OnMoveEvent(InputAction.CallbackContext context)
+    {
+        // อ่านค่าทิศทางออกมาเป็น Vector2 (X แกนซ้าย-ขวา, Y แกนบน-ล่าง)
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+
     void Update()
     {
-        float x = Input.GetAxis("Horizontal"); // WASD
-        float z = Input.GetAxis("Vertical");   // WASD
+       float x = moveInput.x;
+       float z = moveInput.y;
+
+        ////float x = Input.GetAxis("Horizontal"); // WASD
+        ////float z = Input.GetAxis("Vertical");   // WASD
 
         Vector3 forward = cameraTransform.forward;
         Vector3 right = cameraTransform.right;
